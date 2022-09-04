57580c4a83f7bc37068cbc5f6479d3554dae683f
Raafi <rafmhmd06@gmail.com>
2022-09-04 09:45:13 +0700

    + Setup Apollo rest link & auto login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
 import React, { useMemo, useReducer } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+import { RestLink } from 'apollo-link-rest';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import AutoLogin from './components/AutoLogin';
 
 import routes from './routes';
+import cookie from './helpers/cookie';
 import { LocalDataContext } from './core/context';
 import localData from './core/reducers/localData';
 
 export default function App() {
+  const token = cookie.get('access_token');
+  const restLink = new RestLink({
+    uri: 'https://todos-project-api.herokuapp.com',
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  });
+  
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: restLink
+  });
 
-  const [store, dispatch] = useReducer(localData, { } || {});
+  const [store, dispatch] = useReducer(localData, {} || {});
   const contextValue = useMemo(() => ({ store, dispatch }), [store, dispatch]);
 
   return (
-    <LocalDataContext.Provider value={contextValue}>
-      <BrowserRouter>
-        {renderRoutes(routes)}
-      </BrowserRouter>
-    </LocalDataContext.Provider>
+    <ApolloProvider client={client}>
+      <AutoLogin>
+        <LocalDataContext.Provider value={contextValue}>
+          <BrowserRouter>
+            {renderRoutes(routes)}
+          </BrowserRouter>
+        </LocalDataContext.Provider>
+      </AutoLogin>
+    </ApolloProvider>
   )
 }
---
+++ src/components/AutoLogin.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import cookie from '../helpers/cookie';
+import { autoLogin } from '../graphql/queries/autoLogin';
+
+export interface AutoLoginProps {
+  children: any;
+}
+
+export default function AutoLogin(props: AutoLoginProps) {
+  const { children } = props || {};
+
+  useQuery(autoLogin, {
+    onCompleted: ({ autoLogin }) => {
+      cookie.set('access_token', autoLogin.auth_token)
+    }
+  })
+
+  return (
+    <>
+      {
+        cookie.get('access_token') ? (
+          children
+        ) : (
+          <p>Error auto login</p>
+        )
+      }
+    </>
+  )
+}
